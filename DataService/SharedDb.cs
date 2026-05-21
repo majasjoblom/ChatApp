642b39a8095ab78baae42cfd75fd3f8eb0ce2a34
Maja Sjöblom <masj0350@ad.umu.se>
2026-05-21 08:49:15 +0200

    added shareddb.cs

---
+++ DataService/SharedDb.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Concurrent;
+
+namespace ChatApp.DataService
+{
+    public class SharedDb
+    {
+        private readonly ConcurrentDictionary<string, UserConnection> _connection = new();
+
+        public ConcurrentDictionary<string, UserConnection> Connection => _connection;
+    }
+}
